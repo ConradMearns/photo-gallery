78d05312d52be5c8337809a7a8be7bc2846319d7
Conrad Mearns <conradmearns@pm.me>
2023-03-26 22:38:49 -0700

    fix colors

--- indexer.mjs
+++ indexer.mjs
@@ -29,8 +29,8 @@ function rgbToHex({ r, g, b }) {
 }
 
 async function getDominantColor(file) {
-  const metadata = await sharp(file).metadata();
   try {
+    const metadata = await sharp(file).metadata();
     const { dominant } = await sharp(file)
       .resize(Math.ceil(metadata.width / 10), Math.ceil(metadata.height / 10))
       .stats({ channels: ['red', 'green', 'blue'] });
